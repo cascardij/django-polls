b480210679b587565768f6dc1129024cd5bbdb8b
João <joao.cascardi@aluno.ifsp.edu.br>
2023-05-09 11:01:06 -0300

    incluir rota sobre/

--- polls/views.py
+++ polls/views.py
@@ -6,3 +6,7 @@ from django.http import HttpResponse
 #View Index
 def index(request):
     return HttpResponse("Olá, seja bem vindo a enquete")
+
+def sobre(request):
+    return HttpResponse('Este é um app de enquete!')
+    
